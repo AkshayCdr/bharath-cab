056d71d3f2bf45dd5a077e145d92957f9edfbd07
AkshayCdr <akshaychandra418@gmail.om>
2024-07-01 11:49:41 +0530

    added map in final page

---
+++ client/app/component/Ride.tsx
@@ -0,0 +1,58 @@
+import { Form } from "@remix-run/react";
+import { ride } from "apis/ride";
+import React from "react";
+
+export default function RideDetails({
+  rideDetails,
+  sourceName,
+  destinationName,
+}) {
+  console.log(rideDetails);
+
+  return (
+    <div className="flex flex-row">
+      <Form method="POST" id="ride-request-form">
+        <input type="hidden" name="rideId" defaultValue={rideDetails.id} />
+        <p className="ride-details-input">
+          <input
+            type="text"
+            readOnly
+            name="source"
+            defaultValue={(rideDetails.source.x, rideDetails.source.y)}
+            id=""
+            hidden
+          />
+
+          <input
+            type="text"
+            name="destination"
+            id=""
+            readOnly
+            hidden
+            defaultValue={
+              (rideDetails.destination.x, rideDetails.destination.y)
+            }
+          />
+          <label htmlFor="source">Source:</label>
+          <input type="text" name="sourceName" value={sourceName} readOnly />
+          <label htmlFor="destination">Destination:</label>
+          <input
+            type="text"
+            name="destinationName"
+            id=""
+            value={destinationName}
+            readOnly
+          />
+          <label htmlFor="price">Price:</label>
+          <input
+            type="text"
+            name="price"
+            id=""
+            value={rideDetails.price}
+            readOnly
+          />
+        </p>
+      </Form>
+    </div>
+  );
+}
--- client/app/routes/finalPageUser.$rideId.tsx
+++ client/app/routes/finalPageUser.$rideId.tsx
@@ -1,9 +1,9 @@
 import { LoaderFunctionArgs, json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { ride } from "apis/ride";
-import { rideDetails } from "./ride.$rideId";
 import { useEffect, useState } from "react";
 import { socket } from "~/socket/websocket";
+import Ride from "~/component/Ride";
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
   const { rideId } = params;
@@ -22,8 +22,29 @@ export const loader = async ({ params }: LoaderFunctionArgs) => {
 
 export default function FinalPageUser() {
   const [rideLocation, setRideLocation] = useState(null);
+  const [source, setSource] = useState(null);
+  const [destination, setDestination] = useState(null);
+  const [isEditable, setIsEditable] = useState(false);
+  const [MapComponent, setMapComponent] = useState(null);
+
+  const [sourceName, setSourceName] = useState(null);
+  const [destinationName, setDestinationName] = useState(null);
+
   const { rideDetails } = useLoaderData<typeof loader>();
   console.log(rideDetails);
+
+  useEffect(() => {
+    if (rideDetails) {
+      setSource([rideDetails.destination.y, rideDetails.destination.x]);
+      setDestination([rideDetails.source.y, rideDetails.source.x]);
+    }
+  }, [rideDetails]);
+
+  useEffect(() => {
+    import("../component/Map").then((module) =>
+      setMapComponent(() => module.default)
+    );
+  }, []);
 
   useEffect(() => {
     socket.on("updateLocation", (locationData) => {
@@ -32,6 +53,27 @@ export default function FinalPageUser() {
       console.log(latitude, longitude);
       setRideLocation([latitude, longitude]);
     });
-  });
-  return <div>final page user</div>;
+  }, []);
+
+  return (
+    <div>
+      {MapComponent && (
+        <MapComponent
+          source={source}
+          destination={destination}
+          setSource={setSource}
+          setDestination={setDestination}
+          setSourceName={setSourceName}
+          setDestinationName={setDestinationName}
+          isEditable={isEditable}
+          rideLocation={rideLocation}
+        />
+      )}
+      <Ride
+        rideDetails={rideDetails}
+        sourceName={sourceName}
+        destinationName={destinationName}
+      />
+    </div>
+  );
 }
