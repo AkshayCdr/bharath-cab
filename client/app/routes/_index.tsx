512b21a917991045cf22b42ff084441870e112ee
AkshayCdr <akshaychandra418@gmail.om>
2024-07-17 10:25:22 +0530

    Added style in homepage

--- client/app/routes/_index.tsx
+++ client/app/routes/_index.tsx
@@ -14,8 +14,15 @@ export default function Index() {
   return (
     <div>
       <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-        <main className="main-page">
-          <h1 className="main-heading">Bharat Cab</h1>
+        <main className="bg-black h-[80vh] flex justify-between">
+          <h1 className="text-white m-16 p-8 text-6xl font-extrabold">
+            Go anywhere with Bharat Cab
+          </h1>
+          <img
+            src="/public/main-bc.jpg"
+            alt="Getting ride"
+            className="m-10 p-4 mr-44"
+          />
         </main>
       </div>
     </div>
