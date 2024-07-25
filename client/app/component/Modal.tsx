f360e8918a79275abb3c0b28b2ba2914a3f16706
AkshayCdr <akshaychandra418@gmail.om>
2024-07-25 21:02:34 +0530

    Added source name to modal

--- client/app/component/Modal.tsx
+++ client/app/component/Modal.tsx
@@ -1,23 +1,54 @@
+import { useEffect, useState } from "react";
+import { getLocationName } from "~/utils/getLocationName";
+
 export default function Modal({ userData, driverId, onClick }) {
   console.log(userData);
+  const [sourceName, setSourceName] = useState(null);
+  const [destinationName, setDestinationName] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      if (userData) {
+        const source = await getLocationName(
+          userData?.source.y,
+          userData?.source.x
+        );
+        const destination = await getLocationName(
+          userData?.destination.y,
+          userData?.destination.x
+        );
+        setSourceName(source);
+        setDestinationName(destination);
+      }
+    })();
+  }, [userData]);
 
   return (
-    <div className="">
+    <div className="container bg-white max-w-md absolute m-auto left-0 right-0 bottom-1/2 rounded-2xl shadow-md">
       <input type="hidden" name="driverId" defaultValue={driverId} />
       <input type="hidden" name="userId" defaultValue={userData.id} />
       <div className="">
-        <div>
-          <div>$</div>
-          <div>{userData?.price}</div>
+        <div className="flex justify-center text-4xl gap-2 p-3 m-2">
+          <div>Rs.</div>
+          <div>80</div>
+          {/* <div>{userData?.price}</div> */}
         </div>
-        <div>
-          <div>{userData?.source?.x}</div>
-          <div>{userData?.destination?.x}</div>
+        <div className="m-2 p-2">
+          <div>bangaluru karnataka</div>
+          <div>rajapalaya peksra kasjf </div>
+          {/* {sourceName && <div>{sourceName}</div>}
+          {destinationName && <div>{destinationName}</div>} */}
         </div>
       </div>
-      <button type="submit" onClick={onClick}>
-        Accept Ride
-      </button>
+      <div className="flex justify-center m-2 p-2">
+        <button
+          className="bg-black text-white hover:bg-gray-700 font-bold py-2 px-4 rounded-lg"
+          type="submit"
+          onClick={onClick}
+        >
+          Accept Ride
+        </button>
+      </div>
     </div>
   );
 }
