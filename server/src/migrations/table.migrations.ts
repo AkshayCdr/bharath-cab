b20fae14d6901454d1bd178a0d9c45514fb9df90
AkshayCdr <akshaychandra418@gmail.om>
2024-08-13 13:11:12 +0530

    Fixed build eroors

--- server/src/migrations/table.migrations.ts
+++ server/src/migrations/table.migrations.ts
@@ -1,17 +1,13 @@
 import path from "path";
 import fs from "fs";
 import { pool } from "../config/db";
+
 const client = pool.connect();
 
 export async function createSchema(): Promise<void> {
     try {
         const __dirname = path.resolve(".");
-        const filePath = path.join(
-            __dirname,
-
-            "migrations",
-            "createTable.sql"
-        );
+        const filePath = path.join(__dirname, "migrations", "createTable.sql");
         const query = fs.readFileSync(filePath, "utf-8");
         const dbClient = await client;
         await dbClient.query(query);
