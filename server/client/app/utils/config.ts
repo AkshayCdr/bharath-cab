4d10bad1f3cdeda052af02de9c3d157c4ff142a2
AkshayCdr <akshaychandra418@gmail.om>
2024-08-14 15:17:18 +0530

    Change client config

--- server/client/app/utils/config.ts
+++ server/client/app/utils/config.ts
@@ -1,3 +1,7 @@
+// export const config = {
+//     API_KEY: "http://localhost:3000",
+// };
+
 export const config = {
-    API_KEY: "http://localhost:3000",
+    API_KEY: "https://bharat-cab.onrender.com:3000",
 };
