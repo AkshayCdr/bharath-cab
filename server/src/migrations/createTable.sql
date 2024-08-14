a12464dc139973d94d6d6513f32b9fb067ebffc9
AkshayCdr <akshaychandra418@gmail.om>
2024-08-14 14:25:41 +0530

    Changed migration for checking uuid

--- server/src/migrations/createTable.sql
+++ server/src/migrations/createTable.sql
@@ -1,5 +1,13 @@
 BEGIN;
 
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_extension WHERE extname = 'uuid-ossp') THEN
+        CREATE EXTENSION "uuid-ossp";
+    END IF;
+END
+$$;
+
 
 DO $$
 BEGIN
