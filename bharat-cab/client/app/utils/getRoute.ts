3c23d5b7db77c3e70cc58efe519fd66478082419
AkshayCdr <akshaychandra418@gmail.om>
2024-08-15 16:18:02 +0530

    Changed route api

--- bharat-cab/client/app/utils/getRoute.ts
+++ bharat-cab/client/app/utils/getRoute.ts
@@ -1,7 +1,7 @@
 export async function fetchRoutes(source, destination) {
-  const response = await fetch(
-    `http://router.project-osrm.org/route/v1/driving/${source[1]},${source[0]};${destination[1]},${destination[0]}?overview=full&geometries=geojson`
-  );
-  const data = await response.json();
-  return data.routes.length > 0 ? data.routes[0] : null;
+    const response = await fetch(
+        `https://router.project-osrm.org/route/v1/driving/${source[1]},${source[0]};${destination[1]},${destination[0]}?overview=full&geometries=geojson`
+    );
+    const data = await response.json();
+    return data.routes.length > 0 ? data.routes[0] : null;
 }
