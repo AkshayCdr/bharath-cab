9f8af5c209a4459f151f8da014e79a75b2ff075e
AkshayCdr <akshaychandra418@gmail.om>
2024-08-19 16:44:46 +0530

    Added reponsive style to index page

--- bharat-cab/client/app/routes/_index.tsx
+++ bharat-cab/client/app/routes/_index.tsx
@@ -6,32 +6,39 @@ import InputPrice from "~/component/InputPrice";
 import { authLoader } from "~/utils/auth.server";
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+    return [
+        { title: "New Remix App" },
+        { name: "description", content: "Welcome to Remix!" },
+    ];
 };
 
 export const loader = authLoader;
 
 export default function Index() {
-  return (
-    <div>
-      <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-        <main className="bg-gray-950 h-[80vh] flex justify-between">
-          <div className="text-white m-16 p-8">
-            <h1 className=" text-6xl font-bold">Go anywhere with Bharat Cab</h1>
-            <InputPrice />
-          </div>
-          <img
-            src="/main-bc.jpg"
-            alt="Getting ride"
-            className="m-10 p-4 mr-44"
-          />
-        </main>
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <div
+                style={{
+                    fontFamily: "system-ui, sans-serif",
+                    lineHeight: "1.8",
+                }}
+            >
+                <main className="bg-gray-950 flex flex-col lg:flex-row justify-between ">
+                    <div className="text-white m-16 p-8">
+                        <h1 className=" text-6xl font-bold max-w-sm">
+                            Go anywhere with Bharat Cab
+                        </h1>
+                        <InputPrice />
+                    </div>
+                    <img
+                        src="/main-bc.jpg"
+                        alt="Getting ride"
+                        className="m-10 p-10"
+                    />
+                </main>
+            </div>
+        </div>
+    );
 }
 
 export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
