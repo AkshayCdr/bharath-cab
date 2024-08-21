b700f85f4ec3de8789d8632243978b3d0cd1f3a3
AkshayCdr <akshaychandra418@gmail.om>
2024-08-21 12:33:01 +0530

    Server address changed

--- bharat-cab/client/server.js
+++ bharat-cab/client/server.js
@@ -5,19 +5,19 @@ import { createCA, createCert } from "mkcert";
 import * as build from "./build/server/index.js";
 // import cors from "cors";
 
-const ca = await createCA({
-    organization: "Hello CA",
-    countryCode: "NP",
-    state: "Bagmati",
-    locality: "Kathmandu",
-    validity: 365,
-});
+// const ca = await createCA({
+//     organization: "Hello CA",
+//     countryCode: "NP",
+//     state: "Bagmati",
+//     locality: "Kathmandu",
+//     validity: 365,
+// });
 
-const cert = await createCert({
-    ca: { key: ca.key, cert: ca.cert },
-    domains: ["127.0.0.1", "localhost"],
-    validity: 365,
-});
+// const cert = await createCert({
+//     ca: { key: ca.key, cert: ca.cert },
+//     domains: ["127.0.0.1", "localhost"],
+//     validity: 365,
+// });
 
 const app = express();
 // app.use(cors());
@@ -25,18 +25,18 @@ app.use(express.static("build/client"));
 
 app.all("*", createRequestHandler({ build }));
 
-// app.listen(5000, () => {
-//     console.log("App listening on http://localhost:5000");
-// });
-
-const server = https.createServer(
-    {
-        key: cert.key,
-        cert: cert.cert,
-    },
-    app
-);
-
-server.listen(5000, "192.168.0.109", () => {
-    console.log("listening on port 5000");
+app.listen(5000, () => {
+    console.log("App listening on http://localhost:5000");
 });
+
+// const server = https.createServer(
+//     {
+//         key: cert.key,
+//         cert: cert.cert,
+//     },
+//     app
+// );
+
+// server.listen(5000, "192.168.0.109", () => {
+//     console.log("listening on port 5000");
+// });
