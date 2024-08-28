db1d3b297f8f20149456d3cfcf06831e641240d2
AkshayCdr <akshaychandra418@gmail.om>
2024-08-28 21:32:12 +0530

    Changed backend api

--- bharat-cab/client/app/utils/config.ts
+++ bharat-cab/client/app/utils/config.ts
@@ -1,11 +1,11 @@
-export const config = {
-    API_KEY: "http://localhost:3000",
-};
+// export const config = {
+//     API_KEY: "http://localhost:3000",
+// };
 
 // export const config = {
 //     API_KEY: "http://192.168.1.108:3000",
 // };
 
-// export const config = {
-//     API_KEY: "https://bharat-cab.onrender.com",
-// };
+export const config = {
+    API_KEY: "https://bharat-cab.onrender.com",
+};
