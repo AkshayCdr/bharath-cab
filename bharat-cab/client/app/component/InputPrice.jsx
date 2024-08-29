cd86cc232a3fc0838ea26085543e51ccb0d99e8b
AkshayCdr <akshaychandra418@gmail.om>
2024-08-29 16:09:12 +0530

    Fixed input error

--- bharat-cab/client/app/component/InputPrice.jsx
+++ bharat-cab/client/app/component/InputPrice.jsx
@@ -1,5 +1,13 @@
 import { useEffect, useState } from "react";
 
+const getAutoCompleteData = async (input) => {
+    const data = await fetch(
+        `https://api.geoapify.com/v1/geocode/autocomplete?text=${input}&filter=countrycode:auto&apiKey=55b3a098f2a1408d8d10675b02843f50`
+    );
+
+    return data.json();
+};
+
 export default function InputPrice() {
     const [source, setSource] = useState("");
     const [destination, setDestination] = useState("");
@@ -7,20 +15,23 @@ export default function InputPrice() {
     const [sourceCoords, setSourceCoords] = useState([]);
     const [destCoods, setDestCoords] = useState([]);
 
-    const [autoComSource, setAutoComSource] = useState([]);
+    const [autoCompleteSourceData, setAutoComSource] = useState([]);
+    const [isAutoCompleteSource, setIsAutoCompleteSource] = useState(false);
+
+    const [autoCompleteDestData, setAutoCompleteDestData] = useState([]);
+    const [isAutoCompleteDestination, setIsAutoCompleteDestination] =
+        useState(false);
 
     const handleClickSource = (locData) => {
         setSource(locData.name);
         setSourceCoords([locData.lat, locData.lon]);
         setAutoComSource([]);
+        setIsAutoCompleteSource(false);
     };
 
     useEffect(() => {
         if (!source) return;
-        fetch(
-            `https://api.geoapify.com/v1/geocode/autocomplete?text=${source}&filter=countrycode:auto&apiKey=55b3a098f2a1408d8d10675b02843f50`
-        )
-            .then((res) => res.json())
+        getAutoCompleteData(source)
             .then(
                 (result) =>
                     result?.features?.map(({ properties }) => ({
@@ -33,42 +44,106 @@ export default function InputPrice() {
             .catch(console.error);
     }, [source]);
 
+    const handleClickDestination = (locData) => {
+        setDestination(locData.name);
+        setDestCoords([locData.lat, locData.lon]);
+        setAutoCompleteDestData([]);
+        setIsAutoCompleteDestination(false);
+    };
+
+    useEffect(() => {
+        if (!destination) return;
+        getAutoCompleteData(destination)
+            .then(
+                (result) =>
+                    result?.features?.map(({ properties }) => ({
+                        name: properties.formatted,
+                        lon: properties.lon,
+                        lat: properties.lat,
+                    })) || []
+            )
+            .then(setAutoCompleteDestData)
+            .catch(console.error);
+    }, [destination]);
+
+    const handleSeePrices = () => {
+        console.log(sourceCoords);
+        console.log(destCoods);
+    };
+
     return (
         <div className="text-black flex flex-col max-w-[380px] mt-14 gap-3 ">
             <input
                 type="text"
                 name="source"
-                onChange={(e) => setSource(e.target.value)}
+                onFocus={() => setIsAutoCompleteSource(true)}
+                onBlur={() =>
+                    setTimeout(() => setIsAutoCompleteSource(false), 100)
+                }
+                onChange={(e) => {
+                    setSource(e.target.value);
+                    setIsAutoCompleteSource(true);
+                }}
                 className="h-12 rounded-lg text-left px-4"
                 placeholder="Enter location"
                 value={source}
             />
 
-            <div className="dropdown">
-                <ul>
-                    {autoComSource.map((loc, ind) => (
-                        <li
-                            className="dropdown-list"
-                            key={ind}
-                            onClick={() => handleClickSource(loc)}
-                        >
-                            {loc.name}
-                        </li>
-                    ))}
-                </ul>
-            </div>
+            <Dropdown
+                autoCompleteData={autoCompleteSourceData}
+                isAutoComplete={isAutoCompleteSource}
+                handleClick={handleClickSource}
+            />
 
             <input
                 type="text"
                 name="destination"
-                onChange={(e) => setDestination(e.target.value)}
+                onFocus={() => setIsAutoCompleteDestination(true)}
+                onBlur={() =>
+                    setTimeout(() => setIsAutoCompleteDestination(false), 100)
+                }
+                onChange={(e) => {
+                    setDestination(e.target.value);
+                    setIsAutoCompleteDestination(true);
+                }}
                 className="h-12 rounded-lg text-left px-4"
                 placeholder="Enter destination"
+                value={destination}
             />
+
+            <Dropdown
+                autoCompleteData={autoCompleteDestData}
+                isAutoComplete={isAutoCompleteDestination}
+                handleClick={handleClickDestination}
+            />
+
             <div className="dropdown"></div>
-            <button className="text-black bg-white w-32 py-2 px-3 rounded-lg mt-4 hover:bg-slate-300 font-bold cursor-not-allowed">
+            <button
+                onClick={handleSeePrices}
+                className="text-black bg-white w-32 py-2 px-3 rounded-lg mt-4 hover:bg-slate-300 font-bold cursor-not-allowed"
+            >
                 See prices
             </button>
+        </div>
+    );
+}
+
+function Dropdown({ autoCompleteData, isAutoComplete, handleClick }) {
+    return (
+        <div>
+            {isAutoComplete && autoCompleteData.length > 0 && (
+                <div className="dropdown">
+                    <ul>
+                        {autoCompleteData.map((loc, ind) => (
+                            <li className="dropdown-list" key={ind}>
+                                <button onClick={() => handleClick(loc)}>
+                                    {loc.name}
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
         </div>
     );
 }
